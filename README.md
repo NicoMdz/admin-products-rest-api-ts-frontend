11d0ce1044521902e982a2de297ff8ddecaeb6e0
Nicolás Mendez Brito <nick.mdz.b@gmail.com>
2024-06-15 21:41:11 -0600

    Primer Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#  React + Vite + TailwindCSS + TypeScript
+
+### Proyecto: Administrador de Productos
+
+Aplicación dirigida a cualquier usuario que desee llevar un controls y administración de los productos para su empresa. El usuario es capazs de crear, editar, actualizar, eliminar y cambiar la disponibilidad de sus productos. Adicionalmente, el usuario tendrá una vista de una tabla con todos los productos que registre, viendo sus cambios en tiempo real.
+
+### Conocimientos Aplicados
+
+* **Frontend:** React & TypeScript
+* **CCS:** TailwindCSS 
+* **Rutas y Gestión de Formularios:** React Router Dom  
+* **Actions, Loaders, Fetchers**
+---
+**Créditos:** Juan Pablo de la Torre Vázquez <br>
+**Plataforma de enseñanza:** Udemy <br>
+**Curso:** React y TypeScript - La Guía Completa Creando +10 Proyectos
---
+++ src/components/ProductDetails.tsx
@@ -0,0 +1,73 @@
+import { useNavigate, Form, ActionFunctionArgs, redirect, useFetcher } from "react-router-dom"
+import { formatCurrency } from "../helpers"
+import { Product } from "../types"
+import { deleteProduct } from "../services/ProductService"
+
+type ProductDetailsProps = {
+    product: Product
+}
+
+export async function action({params} : ActionFunctionArgs) {
+    if (params.id !== undefined) {
+        await deleteProduct(+params.id)
+        return redirect("/")
+    }
+
+}
+
+export default function ProductDetails({product} : ProductDetailsProps) {
+
+  const fetcher = useFetcher()  
+  const navigate = useNavigate()
+
+  const isAvailable = product.availability
+
+  return (
+    <tr className="border-b ">
+        <td className="p-3 text-lg text-gray-800">
+            {product.name}
+        </td>
+        <td className="p-3 text-lg text-gray-800">
+            {formatCurrency(product.price)}
+        </td>
+        <td className="p-3 text-lg text-gray-800">
+            <fetcher.Form method="POST">
+                <button
+                    type="submit"
+                    name="id"
+                    value={product.id}
+                    className={`${isAvailable ? "text-black" : " text-red-600" } rounded-lg p-2 text-xs uppercase font-bold w-full border border-black-100 hover:cursor-pointer`}
+                > 
+                    {isAvailable ? "Disponible" : "No Disponible"}
+                </button>
+            </fetcher.Form>
+        </td>
+        <td className="p-3 text-lg text-gray-800 ">
+           <div className="flex gap-2 items-center">
+                <button 
+                //No sirve para este ej. ya que el state unicamente se genera al presionar el boton, si alguien intenta entrar por medio de la url sin presionar el boton, no existe el state y la app crashea
+                // onClick={() => navigate(`/productos/${product.id}/editar`, {
+                //     state: {
+                //         product
+                //     }
+                // })}
+                onClick={() => navigate(`/productos/${product.id}/editar`)}
+                className=" bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+                >Editar</button>
+
+                <Form className=" w-full" method="POST" action={`productos/${product.id}/eliminar`} onSubmit={(e) => {
+                    if (!confirm("¿Eliminar?")) {
+                        e.preventDefault()
+                    }
+                }}>
+                    <input 
+                        type="submit" 
+                        value={"Eliminar"}
+                        className=" bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+                    />
+                </Form>
+           </div>
+        </td>
+    </tr> 
+  )
+}
